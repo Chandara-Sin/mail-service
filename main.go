46aff66443b70943a155a7a47f6e1f997b6b101d
Chandara Sin <cdsin68@gmail.com>
2022-11-21 00:01:52 +0700

    feat: add graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"mail-service/mail"
 	"net/http"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -18,9 +23,35 @@ func main() {
 		c.String(http.StatusOK, "Ok v1")
 	})
 
-	r.POST("v1/smtp/mail", mail.SendEmailHandler(mail.SendEmail()))
+	r.POST("/v1/smtp/mail", mail.SendEmailHandler(mail.SendEmail()))
 
-	r.Run(":" + viper.GetString("app.port"))
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + viper.GetString("app.port"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }
 
 func initConfig() {
